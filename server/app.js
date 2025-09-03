2adeedc93bf009e750d9c9b59f8cdc6b31d91838
buihaiduongdev <haiduongtinhoc@gmail.com>
2025-09-03 18:04:40 +0700

    Initial commit: setup project structure, README, .gitignore, .env.example

---
+++ server/app.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use('/', (req, res) => {
+    res.json('Server');
+})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
